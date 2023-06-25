5d460fee04ef7acd3338f9b924646832447c7b1d
bifree <bifree2bfree@gmail.com>
2023-06-26 02:26:29 +0900

    first commit for arukurashi

---
+++ WordPressTheme/footer.php
@@ -0,0 +1,22 @@
+    <footer class="footer">
+      <div class="footer_inner">
+        <div class="footer_address">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/" alt="">
+          <span class="address_small">一般社団法人</span> <span class="footer_name">仕事ノアル暮らし</span>
+          <p class="address">〒489-0819 愛知県瀬戸市西本町2-6プラザビル2F TEL：0561-76-7171</p>
+        </div>
+        <div class="footer_menu">
+          <ul>
+            <li><a href="">ABOUT</a></li>
+            <li><a href="">MAP</a></li>
+            <li><a href="">CONTACT</a></li>
+            <li><a href="">privacy policy</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="copy_right">
+        <p class="copy_right">© 2018-2022 Shigotonoarukurashi</p>
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ WordPressTheme/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Antique+Soft&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
+  </head>
+  <style>
+  @import url('https://fonts.googleapis.com/css2?family=Zen+Antique+Soft&family=Zen+Maru+Gothic:wght@300;400;500&display=swap');
+  </style>
+<body>
+  <header>
+    
+  </header>
+    
