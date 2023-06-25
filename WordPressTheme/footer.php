cb4d1231e2af92f01acd0f19e58ff95075b53f1c
bifree <bifree2bfree@gmail.com>
2023-06-26 03:13:03 +0900

    update top pgge

--- WordPressTheme/footer.php
+++ WordPressTheme/footer.php
@@ -1,9 +1,11 @@
     <footer class="footer">
       <div class="footer_inner">
         <div class="footer_address">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/" alt="">
-          <span class="address_small">一般社団法人</span> <span class="footer_name">仕事ノアル暮らし</span>
+          <div class="footer_logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt=""></div>
+          <div>
+          <p><span class="address_small">一般社団法人</span> <span class="footer_name">仕事ノアル暮らし</span></p>
           <p class="address">〒489-0819 愛知県瀬戸市西本町2-6プラザビル2F TEL：0561-76-7171</p>
+          </div>
         </div>
         <div class="footer_menu">
           <ul>
